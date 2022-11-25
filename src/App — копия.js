38483b8daf9de623fd5d2938df3e0ba3dc55ac73
o6131825 <o6131825@gmail.com>
2022-11-25 19:10:11 +0300

    Init

--- src/App — копия.js
+++ src/App — копия.js
@@ -11,63 +11,36 @@ const arcsData = [...Array(N).keys()].map(() => ({
   endLat: (Math.random() - 0.5) * 180,
   endLng: (Math.random() - 0.5) * 360,
   color: [
-    // ["#B43632", "#EEB649", "#CCD556"][Math.round(Math.random() * 3)],
-    // ["#B43632", "#EEB649", "#CCD556"][Math.round(Math.random() * 3)],
+    ["#B43632", "#EEB649", "#CCD556"][Math.round(Math.random() * 3)],
+    ["#B43632", "#EEB649", "#CCD556"][Math.round(Math.random() * 3)],
   ],
 }));
 
 export default function App() {
   const globeElement = React.useRef();
   const [size, setSize] = React.useState([0, 0]);
-  const [active, setActive] = useState(false);
-  const [rotateSpeed, setRotateSpeed] = useState(1);
-  const [myLat, setMyLat] = useState(53);
-  const [myLng, setMyLng] = useState(1);
-  const [myAltitude, setMyAltitude] = useState(2);
-  const [myautoRotate, setautoRotate] = useState(false);
+  const [rotateSpeed, setRotateSpeed] = useState(0);
 
   React.useEffect(() => {
     // Auto-rotate
-    globeElement.current.controls().autoRotate = myautoRotate;
+    globeElement.current.controls().autoRotate = true;
     globeElement.current.controls().autoRotateSpeed = rotateSpeed;
     globeElement.current.controls().enableZoom = true;
-    globeElement.current.pointOfView({
-      lat: myLat,
-      lng: myLng,
-      altitude: myAltitude,
-    });
-  });
-
-  // React.useEffect(() => {
-  //   // Auto-rotate
-  //   globeElement.current.controls().autoRotate = myautoRotate;
-  //   globeElement.current.controls().autoRotateSpeed = rotateSpeed;
-  //   globeElement.current.controls().enableZoom = true;
-  //   globeElement.current.pointOfView({
-  //     lat: myLat,
-  //     lng: myLng,
-  //   });
-  // }, [myAltitude]);
-
-  // React.useEffect(() => {}, [myLat]);
+  }, [rotateSpeed]);
 
   React.useLayoutEffect(() => {
     setSize([window.innerWidth, window.innerHeight]);
   }, []);
 
-  if (
-    active === true &&
-    // && rotateSpeed >= 0.1
-    myAltitude !== 1 &&
-    myAltitude > 0
-  ) {
-    // setRotateSpeed(0);
-    setTimeout(() => {
-      setRotateSpeed((prev) => 0);
-      setMyAltitude((prev) => (prev * 100 - 1) / 100);
-      setMyLng((prev) => (prev * 100 + 5) / 100);
-    }, 40);
-  }
+  const markers = [
+    {
+      id: "marker1",
+      city: "Singapore",
+      color: "red",
+      coordinates: [1.3521, 103.8198],
+      value: 50,
+    },
+  ];
 
   return (
     <div
@@ -77,21 +50,29 @@ export default function App() {
         position: "relative",
       }}
     >
-      {}
-      <div>
+      <div
+        style={
+          {
+            // transform: "translate(0%, 0%)",
+            // pointerEvents: "none",
+          }
+        }
+      >
         <Globe
           width={size[0]}
           height={size[1]}
-          waitForGlobeReady={true}
+          waitForGlobeReady={false}
           globeImageUrl="/planeta.jpg"
           backgroundImageUrl="/rectangle5.png"
+          arcsData={arcsData}
           arcColor={"color"}
           arcDashLength={() => Math.random()}
           arcDashGap={() => Math.random()}
-          arcStroke={0.3}
+          arcStroke={0.0}
           animateIn={false}
+          arcDashAnimateTime={10000}
           ref={globeElement}
-          enablePointerInteraction={true}
+          enablePointerInteraction={false}
           // markers={markers}
         />
       </div>
@@ -99,18 +80,16 @@ export default function App() {
         style={{
           position: "absolute",
           top: "38%",
-          left: 505,
+          left: 576,
           //left: "2%",
         }}
       >
         <img
-          width="965px"
-          //lat: 53.55, lng: 27.33
+          width="806px"
+          // width="43%"
           src={frame14}
           onClick={() => {
-            setActive(true);
-            // setMyLat(53.55);
-            // setMyLng(27.33);
+            setRotateSpeed(2);
           }}
         />
       </div>
