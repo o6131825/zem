24c1418d3820986d3b5e129fe1c400f0d5171327
o6131825 <o6131825@gmail.com>
2022-11-28 16:10:30 +0300

    Одобрено

--- src/kino.js
+++ src/kino.js
@@ -1,27 +1,23 @@
 import React from "react";
 import Main from "./main";
-import { store } from './store/store'
-import { Provider } from 'react-redux'
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
-import videobg from "./g1.mp4";
-
+//import videobg from "http://localhost:3000/g1.mp4";
 
 export default function Kino() {
-
-
-
-
-
-
-    return (
-
-        <div>
-            <div>
-                <video className="videoTag" autoPlay muted loop>
-                    <source src={videobg} />
-                </video>
-            </div>
-        </div>
-
-    );
+  return (
+    <div className="videoKino">
+      <div>
+        <button className="videoBtn" onClick={() => window.location.reload()}>
+          ✖
+        </button>
+      </div>
+      <div>
+        <video className="videoTag" autoPlay muted loop>
+          <source src="/g1.mp4" />
+        </video>
+      </div>
+    </div>
+  );
 }
--- src/planeta.js
+++ src/planeta.js
@@ -2,16 +2,13 @@ import React, { useState } from "react";
 import frame14 from "./img/frame14.png";
 import frame15_1 from "./img/frame15_1.png";
 import frame15_2 from "./img/frame15_2.png";
+import frame21 from "./img/frame21.png";
 import "./App.css";
 import Globe from "react-globe.gl";
 import { useDispatch } from "react-redux"; // для установки значения
 import { useSelector } from "react-redux"; // для получения значения
-import {
-    setGoFilm,
-  
-} from "./store/mySlice";
+import { setGoFilm } from "./store/mySlice";
 
-import videobg from "./videoplayback.mp4";
 const N = 1000;
 const arcsData = [...Array(N).keys()].map(() => ({
   startLat: (Math.random() - 0.5) * 180,
@@ -30,7 +27,7 @@ export default function Planeta() {
   const [active, setActive] = useState(false);
   const [rotateSpeed, setRotateSpeed] = useState(1);
   const [myLat, setMyLat] = useState(53);
-  const [myLng, setMyLng] = useState(-3);
+  const [myLng, setMyLng] = useState(-2);
   const [myAltitude, setMyAltitude] = useState(2);
   const [myautoRotate, setautoRotate] = useState(false);
   //const [goFilm, setgoFilm] = useState(false);
@@ -41,7 +38,7 @@ export default function Planeta() {
     // Auto-rotate
     globeElement.current.controls().autoRotate = myautoRotate;
     globeElement.current.controls().autoRotateSpeed = rotateSpeed;
-    globeElement.current.controls().enableZoom = true;
+    globeElement.current.controls().enableZoom = false;
     globeElement.current.pointOfView({
       lat: myLat,
       lng: myLng,
@@ -57,7 +54,7 @@ export default function Planeta() {
     active === true &&
     // && rotateSpeed >= 0.1
     myAltitude !== 1 &&
-    myAltitude > 0.5
+    myAltitude > 0.3
   ) {
     // setRotateSpeed(0);
     setTimeout(() => {
@@ -67,11 +64,11 @@ export default function Planeta() {
     }, 40);
   }
 
-  if (active && myAltitude < 0.5) {
+  if (active && myAltitude < 0.3) {
     // setRotateSpeed(0);
     setTimeout(() => {
-        dispatch(setGoFilm(true));
-    }, 3000);
+      dispatch(setGoFilm(true));
+    }, 4000);
   }
 
   return (
@@ -81,14 +78,23 @@ export default function Planeta() {
         background: "#ffffff",
         position: "relative",
       }}
+      onClick={() => {
+        setActive(true);
+      }}
+      onTouchStart={() => {
+        setActive(true);
+      }}
+      onTouchEnd={() => {
+        setActive(true);
+      }}
     >
-  
-      <div>
+      <div className="displayApp"></div>
+      <div className="globeApp">
         <Globe
           width={size[0]}
           height={size[1]}
           waitForGlobeReady={true}
-          globeImageUrl="/planeta5.jpg"
+          globeImageUrl="/planeta5-4.jpg"
           backgroundImageUrl="/rectangle5.png"
           arcColor={"color"}
           arcDashLength={() => Math.random()}
@@ -96,17 +102,11 @@ export default function Planeta() {
           arcStroke={0.3}
           animateIn={false}
           ref={globeElement}
-          enablePointerInteraction={true}
+          enablePointerInteraction={false}
         />
       </div>
       <div className={active ? "m12" : "m1"}>
-        <img
-          width="965px"
-          src={frame14}
-          onClick={() => {
-            setActive(true);
-          }}
-        />
+        <img width="965px" src={frame14} />
       </div>
       <div className={active ? "m22" : "m2"}>
         <img width="400px" src={frame15_1} />
@@ -114,8 +114,8 @@ export default function Planeta() {
       <div className={active ? "m32" : "m3"}>
         <img width="400px" src={frame15_2} />
       </div>
-      <div className={active && myAltitude < 0.5 ? "m4" : "m42"}>
-        <h2>БелВитунифарм</h2>
+      <div className={active && myAltitude < 0.3 ? "m4" : "m42"}>
+        <img width="1000px" src={frame21} />
       </div>
       {/* <div className="test">
         <h2>БелВитунифарм</h2>
